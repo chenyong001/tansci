cbea3c37ff8a2a5e992fc87ccf91e16e8d6a86f4
rui8886 <ruilynnnn@gmail.com>
2023-05-06 10:52:52 +0800

    feat(asrapi.js/env.js): 新增asrAPI和env修改

---
+++ tansci-view/src/api/asrApi.js
@@ -0,0 +1,83 @@
+import axios from '../utils/axios'
+import env from '../config/env'
+
+/**
+ * 获取Azure Token
+ * @param {*} params 
+ * @returns 
+ */
+export function getAzureToken(promptText='',speechText='') {
+    const token = localStorage.getItem('token');
+    if(!token){
+        return Promise.reject()
+    }
+    const url = `${env.host.base}/tansci/collect/getAzureToken`
+    return axios.post(url, {},{
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;',
+            'Authorization':`Bearer ${token}`
+        },
+    });
+}
+
+
+/**
+ * ASR 创建Note
+ * @param {*} params 
+ * @returns 
+ */
+export function createNote({sessionId,remark}) {
+    const token = localStorage.getItem('token');
+    if(!token){
+        return Promise.reject()
+    }
+    const url = `${env.host.base}/tansci/collect/createNote?docId=${sessionId}&remark=${remark}`
+    return axios.get(url, {},{
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;',
+            'Authorization':`Bearer ${token}`
+        },
+    });
+}
+
+/**
+ * ASR 发送Note
+ * @param {*} params 
+ * @returns 
+ */
+export function sendNote({resultText,language,sessionId,offset,duration}) {
+    const token = localStorage.getItem('token');
+    if(!token){
+        return Promise.reject()
+    }
+    const url = `${env.host.base}/tansci/collect/sendNote?resultText=${resultText}&language=${language}&docId=${sessionId}&offset=${offset}&duration=${duration}`
+    return axios.get(url, {},{
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;',
+            'Authorization':`Bearer ${token}`
+        },
+    });
+}
+
+/**
+ * 上传录音
+ * @param {*} params 
+ * @returns 
+ */
+export function uploadFile({fileName,mp3Blob,sessionId}) {
+    const token = localStorage.getItem('token');
+    if(!token){
+        return Promise.reject()
+    }
+    const url = `${env.host.base}/tansci/collect/upload`
+    const data = new FormData()
+    const mp3Name = encodeURIComponent(fileName + '.mp3');
+    data.append('fileName', mp3Name);
+    data.append('file', mp3Blob);
+    data.append('docId', sessionId);
+    return axios.post(url, data,{
+        headers: {
+            'Authorization':`Bearer ${token}`
+        },
+    });
+}
--- tansci-view/src/config/env.js
+++ tansci-view/src/config/env.js
@@ -1,5 +1,5 @@
 const host = {
-    base:'//opencast.tsi.edu.sg'
+    base:'' // //opencast.tsi.edu.sg
 }
 console.log('env.MODE-',import.meta.env.MODE)
 if(import.meta.env.MODE === "development"){
