ab2160698028445cdf0010ece6740f0769ea60b2
rui8886 <ruilynnnn@gmail.com>
2023-05-25 19:46:43 +0800

    refactor(env.js): 还原asrbase域名

--- tansci-view/src/config/env.js
+++ tansci-view/src/config/env.js
@@ -1,6 +1,6 @@
 const host = {
     base:'',// //opencast.tsi.edu.sg
-    asrBase:'//opencast.tsi.edu.sg'
+    asrBase:''
 }
 console.log('env.MODE-',import.meta.env.MODE)
 if(import.meta.env.MODE === "development"){
